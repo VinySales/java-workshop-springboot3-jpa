be8d1232d7084ccc6974c6981d13c30837f52705
Vinicius Sales Rebouças <vinysalesreboucas@hotmail.com>
2023-03-16 01:03:01 -0300

    Category: Entity, Service, Resource

--- src/main/java/com/devsuperior/course/resources/OrderResource.java
+++ src/main/java/com/devsuperior/course/resources/OrderResource.java
@@ -22,7 +22,6 @@ public class OrderResource {
 	@GetMapping
 	public ResponseEntity<List<Order>> findAll(){
 		List<Order> list = service.findAll();
-		
 		return ResponseEntity.ok().body(list);
 	}
 	
