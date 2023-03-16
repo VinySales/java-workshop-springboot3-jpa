5792ae91c8198796a3d99d4817c10f70a30812f8
Vinicius Sales Rebouças <vinysalesreboucas@hotmail.com>
2023-03-16 00:05:27 -0300

    classes order

---
+++ src/main/java/com/devsuperior/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
