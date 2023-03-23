8edd4649322811fd8142df987738137a49ed7b27
VinySales <vinysalesreboucas@hotmail.com>
2023-03-22 21:27:39 -0300

    add category

--- src/main/java/com/devsuperior/course/config/TestConfig.java
+++ src/main/java/com/devsuperior/course/config/TestConfig.java
@@ -45,6 +45,7 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		Category cat4 = new Category(null, "Mouses");
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
