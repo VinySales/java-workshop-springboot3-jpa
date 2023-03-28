39acc0bada00dd89681890ab1e914bd93600c23b
VinySales <vinysalesreboucas@hotmail.com>
2023-03-27 23:31:26 -0300

    Entidade Payment

--- src/main/java/com/devsuperior/course/config/TestConfig.java
+++ src/main/java/com/devsuperior/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devsuperior.course.entities.Category;
 import com.devsuperior.course.entities.Order;
 import com.devsuperior.course.entities.OrderItem;
+import com.devsuperior.course.entities.Payment;
 import com.devsuperior.course.entities.Product;
 import com.devsuperior.course.entities.User;
 import com.devsuperior.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
